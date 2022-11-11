1c19043beeac0b70f0c337835dd2da60328d9c08
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-11 22:23:53 +0900

    useCallback으로 최적화

--- src/Average.js
+++ src/Average.js
@@ -41,7 +41,50 @@
 
 // 인풋 내용이 바뀔 때는 평균값 다시 계산할 필요가 없으므로 이 부분을 최적화.
 // 렌더링 과정 중에서 특정 값이 바뀌었을 때만 연산 실행, 원하는 값이 안 바뀌었으면 이전 연산 결과 그대로 쓰기.
-import React, {useState, useMemo} from 'react';
+// import React, {useState, useMemo} from 'react';
+
+// const getAverage = numbers => {
+//     console.log('평균값 계산 중..');
+//     if (numbers.length === 0) return 0;
+//     const sum = numbers.reduce((a, b) => a + b);
+//     return sum / numbers.length;
+// };
+
+// const Average = () => {
+//     const [list, setList] = useState([]);
+//     const [number, setNumber] = useState('');
+
+//     const onChange = e => {
+//         setNumber(e.target.value);
+//     };
+//     const onInsert = e => {
+//         const nextList = list.concat(parseInt(number));
+//         setList(nextList);
+//         setNumber('');
+//     };
+
+//     const avg = useMemo(() => getAverage(list), [list]);
+    
+//     return (
+//         <>
+//             <input value={number} onChange={onChange} />
+//             <button onClick={onInsert}>등록</button>
+//             <ul>
+//                 {list.map((value, index) => (
+//                     <li key={index}>{value}</li>
+//                 ))}
+//             </ul>
+//             <>
+//                 <b>평균값:</b> {avg}
+//             </>
+//         </>
+//     );
+// };
+
+// export default Average;
+
+// useCallback: useMemo와 유사. 만들어놨던 함수를 재사용할 수 있게 해줌.
+import React, {useState, useMemo, useCallback} from 'react';
 
 const getAverage = numbers => {
     console.log('평균값 계산 중..');
@@ -54,14 +97,15 @@ const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
 
-    const onChange = e => {
+    const onChange = useCallback(e => {
         setNumber(e.target.value);
-    };
-    const onInsert = e => {
+    }, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
+
+    const onInsert = useCallback(() => {    
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    };
+    }, [number, list]); // nunber or list가 바뀌었을 때만 함수 생성
 
     const avg = useMemo(() => getAverage(list), [list]);
     
