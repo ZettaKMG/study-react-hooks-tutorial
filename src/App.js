a7c4fc6a42a0bd22095cf1f887d5798dfdb1992a
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-11 21:55:47 +0900

    useReducer로 카운터 구현한 것을 렌더링

--- src/App.js
+++ src/App.js
@@ -19,24 +19,34 @@
 // export default App;
 
 // useEffect 뒷정리 함수 렌더링
-import React, {useState} from 'react';
-import Info from './Info';
+// import React, {useState} from 'react';
+// import Info from './Info';
+
+// const App = () => {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <>
+//       <button
+//         onClick={() => {
+//           setVisible(!visible);
+//         }}
+//       >
+//         {visible ? '숨기기' : '보이기'}  
+//       </button>  
+//       <hr />
+//       {visible && <Info />}
+//     </>
+//   );
+// };
+
+// export default App;
+
+// useReducer로 카운터 구현한 것을 렌더링
+import React from 'react';
+import Counter from './Counter';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}  
-      </button>  
-      <hr />
-      {visible && <Info />}
-    </>
-  );
+  return <Counter />;
 };
 
 export default App;
